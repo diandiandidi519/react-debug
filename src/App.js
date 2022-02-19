a6e8e38e6f54c9ee3d92d4d09a685c604f2ef3b2
wangshuya <wangshuya.wsy@bytedance.com>
2022-02-19 17:07:05 +0800

    做的标记

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// function App() {
+//   const handleClick = (value) => console.log(value);
+//   return (
+//     <div className="container" onClick={handleClick}>
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>
+//         Edit <code>src/App.js</code> and save to reload.
+//       </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn React
+//       </a>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// console.log(document.getElementById("root")._reactRootContainer);
+// class AppClass extends React.Component {
+//   render() {
+//     return <p>KaSong</p>;
+//   }
+// }
+// console.log("这是ClassComponent：", AppClass);
+// console.log("这是Element：", <AppClass />);
+
+// function AppFunc() {
+//   return <p>KaSong</p>;
+// }
+// console.log("这是FunctionComponent：", AppFunc);
+// console.log("这是Element：", <AppFunc />);
+
+class App extends React.Component {
+  state = { number: 0 };
+  handleClick = () => {
+    debugger;
+    this.setState({ number: this.state.number + 1 }, () => {
+      console.log("callback1", this.state.number);
+    });
+    console.log(this.state.number);
+    this.setState({ number: this.state.number + 1 }, () => {
+      console.log("callback2", this.state.number);
+    });
+    console.log(this.state.number);
+    this.setState({ number: this.state.number + 1 }, () => {
+      console.log("callback3", this.state.number);
+    });
+    console.log(this.state.number);
+  };
+  componentDidMount() {
+    // debugger;
+  }
+  render() {
+    return (
+      <div>
+        {this.state.number}
+        <button onClick={this.handleClick}>number++</button>
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+debugger
+{/* <React.StrictMode> */}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
+  {/* </React.StrictMode>, */}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
